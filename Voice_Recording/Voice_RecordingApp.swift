d4d9710f1b2048412eb25e0474d82caf6a631079
jooeun kim <jay311311@gmail.com>
2023-11-28 23:02:13 +0900

    Initial Commit

---
+++ Voice_Recording/Voice_RecordingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_RecordingApp.swift
+//  Voice_Recording
+//
+//  Created by Jooeun Kim on 2023/11/28.
+//
+
+import SwiftUI
+
+@main
+struct Voice_RecordingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
